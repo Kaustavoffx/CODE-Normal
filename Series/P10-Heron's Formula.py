ae8f13a5d665dfeca297f7ec04d8e155ff68bbe0
Kaustavoffx <kaustav.chowdhury9999@gmail.com>
2026-04-05 15:01:21 +0530

    corrected P-10 added

--- Series/P10-Heron's Formula.py
+++ Series/P10-Heron's Formula.py
@@ -1,16 +1,13 @@
-a=float(input("Enter the length of first side of the triangle: "))
-b=float(input("Enter the length of second side of the triangle: "))
-c=float(input("Enter the length of third side of the triangle: "))
+a = float(input("Enter the length of first side of the triangle: "))
+b = float(input("Enter the length of second side of the triangle: "))
+c = float(input("Enter the length of third side of the triangle: "))
 
-s=(a+b+c) /2
+s = (a + b + c) / 2
 
-#Area of the triangle using Heron's formula
-area =(s* (s-a)*(s-b)*(s-c))
+# Area of the triangle using Heron's formula (Added the square root ** 0.5)
+area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
 
 print(f"Area of the triangle: {area} unit²")
 
-#output
-# Enter the length of first side of the triangle: 7
-# Enter the length of second side of the triangle: 8
-# Enter the length of third side of the triangle: 9
-# Area of the triangle: 720.0 unit²
+# Corrected Output for 7, 8, 9:
+# Area of the triangle: 26.832815729997476 unit²
