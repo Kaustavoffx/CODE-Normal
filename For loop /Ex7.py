f783efe08d1fe0a1fda536cc229e667c9c908203
Kaustav Chowdhury <kaustav.chowdhury9999@gmail.com>
2026-04-21 21:07:35 +0530

    Create Ex7.py

---
+++ For loop /Ex7.py
@@ -0,0 +1,7 @@
+count=0
+print("Numbers divisible by 7 from 1 to 100:")
+for i in range(1,101):
+    if i%7==0:
+        print(i,end=" ")
+        count=count+1
+print("\nTotal numbers divisible by 7:",count)
